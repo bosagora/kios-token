14826358fa17b4c2d98d89c84ead57c6860bebcd
Michael Kim <michael.kim.200518@gmail.com>
2024-01-27 15:18:58 +0900

    Add a method transferWithSignature

--- packages/contracts/src/utils/ContractUtils.ts
+++ packages/contracts/src/utils/ContractUtils.ts
@@ -1,12 +1,23 @@
 // tslint:disable-next-line:no-implicit-dependencies
-import { Interface } from "@ethersproject/abi";
+import { defaultAbiCoder, Interface } from "@ethersproject/abi";
+// tslint:disable-next-line:no-implicit-dependencies
+import { Signer } from "@ethersproject/abstract-signer";
+// tslint:disable-next-line:no-implicit-dependencies
+import { BigNumber, BigNumberish } from "@ethersproject/bignumber";
+// tslint:disable-next-line:no-implicit-dependencies
+import { arrayify, BytesLike } from "@ethersproject/bytes";
+// tslint:disable-next-line:no-implicit-dependencies
+import { AddressZero } from "@ethersproject/constants";
 // tslint:disable-next-line:no-implicit-dependencies
 import { ContractReceipt, ContractTransaction } from "@ethersproject/contracts";
 // tslint:disable-next-line:no-implicit-dependencies
 import { id } from "@ethersproject/hash";
 // tslint:disable-next-line:no-implicit-dependencies
+import { keccak256 } from "@ethersproject/keccak256";
+// tslint:disable-next-line:no-implicit-dependencies
 import { Log } from "@ethersproject/providers";
-import { BigNumber } from "ethers";
+// tslint:disable-next-line:no-implicit-dependencies
+import { verifyMessage } from "@ethersproject/wallet";
 
 export class ContractUtils {
     /**
@@ -77,5 +88,27 @@ export class ContractUtils {
             return parsedLog.args[field];
         }
         return undefined;
+    }
+
+    public static getTransferMessage(from: string, to: string, amount: BigNumberish, nonce: BigNumberish): Uint8Array {
+        const encodedResult = defaultAbiCoder.encode(
+            ["address", "address", "uint256", "uint256"],
+            [from, to, amount, nonce]
+        );
+        return arrayify(keccak256(encodedResult));
+    }
+
+    public static async signMessage(signer: Signer, message: Uint8Array): Promise<string> {
+        return signer.signMessage(message);
+    }
+
+    public static verifySignature(message: Uint8Array, signature: BytesLike): string {
+        let res: string;
+        try {
+            res = verifyMessage(message, signature);
+        } catch (error) {
+            return AddressZero;
+        }
+        return res;
     }
 }
